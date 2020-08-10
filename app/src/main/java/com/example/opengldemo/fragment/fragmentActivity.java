87201a47110771a0b8e5f97620b380e0ce1bafbb
HitLumino <1095916953@qq.com>
2020-08-11 01:32:55 +0800

    fragment + viewpager

---
+++ app/src/main/java/com/example/opengldemo/fragment/fragmentActivity.java
@@ -0,0 +1,26 @@
+package com.example.opengldemo.fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.opengldemo.R;
+
+public class fragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        MyFragment myFragment = new MyFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment, myFragment).commit();
+
+    }
+
+}
